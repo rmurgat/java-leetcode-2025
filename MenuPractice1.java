58bff77fc5dffafd1e89c6d3fa129ffb055c9fa8
Ruben Murga <ruben.murgat@gmail.com>
2024-12-06 21:07:34 -0600

    updating code

--- MenuPractice1.java
+++ MenuPractice1.java
@@ -11,15 +11,38 @@ class MenuPractice1 {
         System.out.println("Sum Two: " + Arrays.toString(lib.twoSum(array, 9)) );
     }
 
+    public void printingContainsDuplicate() {
+        ListBundle01 lib = new ListBundle01();
+        int[] array = new int[] {2,7,11,15};
+        System.out.println("[ Printing Contains Duplicate ]");
+        System.out.println("Answer No.1: " + lib.hasDuplicate(array) );
+        array = new int[] {2,7,11,11,15};
+        System.out.println("Answer No.2: " + lib.hasDuplicate(array) );
+    }
+
+    public void printingValidAnagram() {
+        ListBundle01 lib = new ListBundle01();
+        System.out.println("[ Valid Anagram ]");
+        System.out.println("Answer No. 1: " + lib.isAnagram("anagram", "nagaram"));
+    }
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         MenuPractice1 app = new MenuPractice1();
         System.out.println("Menu Practice Java No. 1");
-        System.out.println("1. Sum Two");
+        System.out.println("1. Sum Two (LC#1)");
+        System.out.println("2. Contains Duplicate (LC#217)");
+        System.out.println("3. Valid Anagram (LC#242)");
         int opc = keyboard.nextInt();
         switch (opc) {
             case 1:
                 app.printingSumTwo();
+                break;
+            case 2:
+                app.printingContainsDuplicate();
+                break;
+            case 3:
+                app.printingValidAnagram();
                 break;
         
             default:
--- solutions/ListBundle01.java
+++ solutions/ListBundle01.java
@@ -1,6 +1,11 @@
 package solutions;
 
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ListBundle01 {
 
@@ -15,5 +20,40 @@ public class ListBundle01 {
             memo.put(nums[i], i);
         }
         return null;
+    }
+
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int L = nums.length;
+        for(int i=0; i<L; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }    
+
+    public boolean isAnagram(String s, String t) {
+        Hashtable<Character, Integer> memo = new Hashtable<>();
+        for (Character c : s.toCharArray()) {
+            Integer located = memo.get(c);
+            if(located!=null) {
+                memo.put(c, located+1);
+            } else {
+                memo.put(c, 1);
+            }
+        }
+        for (Character c: s.toCharArray()) {
+            Integer located = memo.get(c);
+            if(located!=null) {
+                memo.put(c, located-1);
+            } else {
+                return false;
+            }
+        }
+        List<Integer> res = memo.values().stream().filter(x -> x!=0).collect(Collectors.toList());
+        if (res.isEmpty())  return true;
+        return false;
     }
 }
