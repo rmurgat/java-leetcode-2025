b619a068b822085b6c3868cd8c2a20e7b17b155c
Ruben Murga <ruben.murgat@gmail.com>
2025-01-30 09:30:46 -0600

    resetting java solutions

--- MenuPractice1.java
+++ MenuPractice1.java
@@ -6,156 +6,72 @@ import solutions.ListBundle01;
 
 class MenuPractice1 {
 
-    public void printingSumTwo() {
-        ListBundle01 lib = new ListBundle01();
-        int[] array = new int[] {2,7,11,15};
-        System.out.println("[ Printing Sum Two ]");
-        System.out.println("Sum Two: " + Arrays.toString(lib.twoSum(array, 9)) );
-    }
-
     public void printingContainsDuplicate() {
         ListBundle01 lib = new ListBundle01();
-        int[] array = new int[] {2,7,11,15};
-        System.out.println("[ Printing Contains Duplicate ]");
-        System.out.println("Answer No.1: " + lib.hasDuplicate(array) );
-        array = new int[] {2,7,11,11,15};
-        System.out.println("Answer No.2: " + lib.hasDuplicate(array) );
+        System.out.println(" [ 217. Contains Duplicate ]");
+        System.out.println(" Answer 1: " + (lib.hasDuplicate_1(new int[] {1, 2, 3, 3})?"true":"false"));
+        System.out.println(" Answer 2: " + (lib.hasDuplicate_2(new int[] {1, 2, 3, 3})?"true":"false"));
+        System.out.println(" Answer 3: " + (lib.hasDuplicate_3(new int[] {1, 2, 3, 3})?"true":"false"));
+        System.out.println(" Answer 4: " + (lib.hasDuplicate_3(new int[] {1, 2, 3, 4})?"true":"false"));
     }
 
+    public void printingSumTwo() {
+        ListBundle01 lib = new ListBundle01();
+        System.out.println(" [1. Two Sum ] ");
+        System.out.printf(" Answer 1: ");
+        int[] res = lib.twoSum(new int[] {2,7,11,15}, 9);
+        if (res != null)
+            for (int i: res) 
+                System.out.printf(" %d \n", i);
+        else
+            System.out.println("null returned");
+    }
     public void printingValidAnagram() {
         ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Valid Anagram ]");
-        System.out.println("Answer No. 1: " + lib.isAnagram("anagram", "nagaram"));
+        System.out.println(" [ 242. Valid Anagram ]");
+        System.out.println(" Answer 1 :" + (lib.isAnagram("anagram","nagaram")? "true":"false"));
+        System.out.println(" Answer 1 :" + (lib.isAnagram("angram","nagaram")? "true":"false"));
     }
 
     public void printingGroupAnagram() {
         ListBundle01 lib = new ListBundle01();
-        String[] input =  {"eat","tea","tan","ate","nat","bat"};
-        System.out.println("[ Group Anagram ]");
-        List<List<String>> res = lib.groupAnagrams(input);
-        System.out.println("Answer No. 1: ");
-        res.forEach(System.out::println);
-    }
-
-    public void printingReplaceElement() {
-        ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Replace Elements ]");
-        System.out.println("Answer No. 1: ");
-        int[] input = new int[] {17,18,5,4,6,1};
-        int[] ans = lib.replaceElements(input);
-        Arrays.stream(ans).forEach(x->System.out.printf("%d,",x));
-    }
-    public void printingIsSubsequence() {
-        ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Is Subsequence ]");
-        System.out.println("Answer No. 1: " + lib.isSubsequence("abc", "ahbgdc"));
-    }
-    public void printingLengthLastWord() {
-        ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Length of Last Word ]");
-        System.out.printf("Answer No. 1: %d\n", lib.lengthOfLastWord("   fly me   to   the moon  "));
-        System.out.printf("Answer No. 2: %d\n", lib.lengthOfLastWord("Hello World"));
-        System.out.printf("Answer No. 3: %d\n", lib.lengthOfLastWord("luffy is still joyboy"));
-        System.out.printf("Answer No. 4: %d\n", lib.lengthOfLastWord("luffy"));
-        System.out.printf("Answer No. 5: %d\n", lib.lengthOfLastWord(""));
-    }
-    public void printingLongestCommonPrefix() {
-        ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Longest Common Prefix ]");
-        System.out.println ("Answer No. 1, type=1: " + lib.longestCommonPrefix_1(new String[] {"flower","flow","flight"}));
-        System.out.println ("Answer No. 2, type=1: " + lib.longestCommonPrefix_1(new String[] {"dog","racecar","car"}));
-        System.out.println ("Answer No. 3, type=1: " + lib.longestCommonPrefix_1(new String[] {"car"}));
-        System.out.println ("Answer No. 4, type=1: " + lib.longestCommonPrefix_1(new String[] {""}));
-        System.out.println ("Answer No. 1, type=2: " + lib.longestCommonPrefix_2(new String[] {"flower","flow","flight"}));
-        System.out.println ("Answer No. 2, type=2: " + lib.longestCommonPrefix_2(new String[] {"dog","racecar","car"}));
-        System.out.println ("Answer No. 3, type=2: " + lib.longestCommonPrefix_2(new String[] {"car"}));
-        System.out.println ("Answer No. 4, type=2: " + lib.longestCommonPrefix_2(new String[] {""}));
-    }
-    public void printingTopKFequentElement() {
-        ListBundle01 lib = new ListBundle01();
-        System.out.println("[ Top K Frequent Element ]");
-        System.out.println (" Answer 1, type 1 [");
-        int[] res1 = lib.topKFrequent(new int[] {1,1,1,2,2,3},2);
-        Arrays.stream(res1).forEach(System.out::println);
-        System.out.println ("] Answer 2, type 1 [");
-        int[] res2 = lib.topKFrequent(new int[] {1},1);
-        Arrays.stream(res2).forEach(System.out::println);
-    }
-
-    public void printingEncodeDecodeStrArray() {
-        ListBundle01 lib = new ListBundle01();
-        List<String> a = new ArrayList<>( List.of("neet","code","love","you"));
-
-        System.out.println(" [ Encode Decode String Array ]");
-        String encoded = lib.encode(a);
-        System.out.println(" Encode : " + lib.encode(a));
-        List<String> d = lib.decode(encoded);
-        for (String s: d) {
-            System.out.println(s);
-        }
-    }
-
-    public void printingArrayExceptSelf() {
-        ListBundle01 lib = new ListBundle01();
-        int[] nums = new int[]{1,2,3,4};
-        System.out.println(" [ Array Except Self ]");
-        int[] ans = lib.productExceptSelf(nums);
-        System.out.println(" Answer No. 1: ");
-        for(int i = 0; i<ans.length; i++) { 
-            System.out.println(ans[i]);
-        }
+        String[] input = new String[]{"eat","tea","tan","ate","nat","bat"};
+        System.out.println(" Answer 1 :" + lib.groupAnagrams(input));
     }
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         MenuPractice1 app = new MenuPractice1();
         System.out.println("Menu Practice Java No. 1");
-        System.out.println("1. Sum Two (LC#1)");
-        System.out.println("2. Contains Duplicate (LC#217)");
-        System.out.println("3. Valid Anagram (LC#242)");
-        System.out.println("4. Group Anagram (LC#49)");
-        System.out.println("5. Replace Elements (LC#1299)");
-        System.out.println("6. Is Subsequence (LC#392)");
-        System.out.println("7. Length of Last Word (LC#58)");
-        System.out.println("8. Longest Common Prefix (LC#14)");
-        System.out.println("9. Top K Frequent Element");
-        System.out.println("10. Encode and Decode String (neetcode)");
-        System.out.println("11. Product of Array Except Self (LC#238)");
+        System.out.println("1. Two Sum (LC#1)                         21. Maximum Number of Ballons (LC#1189)      41. Koko Eating Bananas (LC#875)");
+        System.out.println("2. Contains Duplicate (LC#217)            22. Word Patten (LC#290)                     42. Reverse Linked List (LC#206)");
+        System.out.println("3. Valid Anagram (LC#242)                 23. Design HashSet (LC#705)                  43. Merge Two Sorted List (LC#21)");
+        System.out.println("4. Group Anagram (LC#49)                  24. Design HashMap (LC#706)                  44. Linked List Cycle Detection (LC#141)");
+        System.out.println("5. Replace Elements (LC#1299)             25. Two Integer Sum II (LC#167)              45. Reorder Linked List (LC#143)");
+        System.out.println("6. Is Sequence (LC#392)                   26. Longest Consecutive Sequence (LC#128)    46. Remove Node From End of Linked List (LC#19)");
+        System.out.println("7. Length of Last Word (LC#58)            27. Trapping Rain Water (LC#42)              47. Copy Linked List with Random Pointer(LC#138)");
+        System.out.println("8. Longest Common prefix (lc#14)          28. Best Time to Buy and Sell Stock (LC#121) 48. Find Minimum in Rotated Sorted Array(LC#153)");
+        System.out.println("9. Top K Frequent Element (LC#347)        29. Longest substr Without Rep Chars(LC#3)   49. Search in Rotated Sorted Array (LC#33)");
+        System.out.println("10. Encode and Decode String (neetcode)   30. Longest Repeat Char Replacement (LC#424) 50. Invert Binary Tree (LC#226)");
+        System.out.println("11. Product of Array Except Self (LC#238) 31. Valid Parentheses (LC#20)                51. Maximum Depth of Binary Tree (LC#111)");
+        System.out.println("12. Pascal's Triangle (LC#118)            32. Minimum Stack (LC#155)                   52. Diameter of Binary Tree (LC#543)");
+        System.out.println("13. Unique Email Addresses (LC#929)       33. Generate Parentheses (LC#22)             53. Balanced Binary Tree (LC#110)");
+        System.out.println("14. Isomorphic String (LC#205)            34. Daily Temperatures (LC#739)              54. Subtree of Another Tree (LC#572)");
+        System.out.println("15. Can Place Flowers (LC#605)            35. Binary Search (LC#704)                   55. Valid Sudoku (LC#36)");
+        System.out.println("16. Majority Element (LC#169)             36. Search a 2D Matrix (LC#74)               56. Sliding Window Maximum (LC#239)");
+        System.out.println("17. Next Greater Element I (LC#496)       37. Evaluate Reverse Polish Notation(LC#150) 57. Car Fleet (LC#853)");
+        System.out.println("18. Find Pivot Index (LC#724)             38. Permutation in String (LC#567)           58. Time Based Key-Value Store (LC#981)");
+        System.out.println("19. Sum Numpy Array Range (LC#303)        39. Minimum Window Substring                 59. Copy Linked List with Random Pointer(LC#138)");
+        System.out.println("20. Find All Numbers Disappeared (LC#448) 40. Monotonic Stack (practice)               60. Find the Duplicate Number (LC#287)");
+    
         int opc = keyboard.nextInt();
         switch (opc) {
-            case 1:
-                app.printingSumTwo();
-                break;
-            case 2:
-                app.printingContainsDuplicate();
-                break;
-            case 3:
-                app.printingValidAnagram();
-                break;
-            case 4:
-                app.printingGroupAnagram();
-                break;
-            case 5:
-                app.printingReplaceElement();
-                break;
-            case 6: 
-                app.printingIsSubsequence();
-                break;
-            case 7:
-                app.printingLengthLastWord();
-                break;
-            case 8:
-                app.printingLongestCommonPrefix();
-                break;
-            case 9: 
-                app.printingTopKFequentElement();
-                break;
-            case 10:
-                app.printingEncodeDecodeStrArray();
-            case 11:
-                app.printingArrayExceptSelf();
-            default:
-                break;
+            case 1: app.printingSumTwo(); break;
+            case 2: app.printingContainsDuplicate(); break;
+            case 3: app.printingValidAnagram(); break;
+            case 4: app.printingGroupAnagram(); break;
+
+            default: break;
         }
         keyboard.close();
     }
--- solutions/ListBundle01.java
+++ solutions/ListBundle01.java
@@ -2,213 +2,88 @@ package solutions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ListBundle01 {
 
+    /* LC#1 - Two Sum */
     public int[] twoSum(int[] nums, int target) {
-        Hashtable<Integer, Integer> memo = new Hashtable<>();
+        Hashtable<Integer, Integer> ht = new Hashtable<>();
         int L = nums.length;
-        for(int i=0; i<L; i++) {
-            Integer located = memo.get(Integer.valueOf(target-nums[i]));
-            if( located != null ) {
+        for (int i=0; i<L; i++) {
+            Integer located = ht.get(Integer.valueOf(target-nums[i]));
+            if (located != null) {
                 return new int[] { i, located };
             }
-            memo.put(nums[i], i);
+            ht.put(i, nums[i]);
         }
         return null;
     }
 
-    public boolean hasDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int L = nums.length;
-        for(int i=0; i<L; i++) {
-            if (set.contains(nums[i])) {
-                return true;
-            }
-            set.add(nums[i]);
+    public boolean hasDuplicate_1(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i-1]==nums[i]) return true;
         }
         return false;
-    }    
+    }
+    public boolean hasDuplicate_2(int[] nums) {
+        Set<Integer> s = null;
+        s  = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return s.size()!=nums.length;
+    }
+    public boolean hasDuplicate_3(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i: nums) list.add(Integer.valueOf(i));
+        Integer last = 0;
+        for(Integer i: list) {
+            if (i==last) return true;
+            last = i;
+        }
+        return false;
+    }
 
     public boolean isAnagram(String s, String t) {
-        Hashtable<Character, Integer> memo = new Hashtable<>();
-        for (Character c : s.toCharArray()) {
-            Integer located = memo.get(c);
-            if(located!=null) {
-                memo.put(c, located+1);
-            } else {
-                memo.put(c, 1);
-            }
+        Hashtable<Character, Integer>  ht = new Hashtable<>();
+        for(Character c: s.toCharArray()) {
+            Integer located = ht.get(c);
+            if (located!=null) {
+                ht.replace(c, located+1);
+            } else
+                ht.put(c, 1);
         }
-        for (Character c: s.toCharArray()) {
-            Integer located = memo.get(c);
-            if(located!=null) {
-                memo.put(c, located-1);
-            } else {
+        for(Character c: t.toCharArray()) {
+            Integer located = ht.get(c);
+            if (located!=null) {
+                ht.replace(c, located-1);
+            } else
                 return false;
-            }
         }
-        List<Integer> res = memo.values().stream().filter(x -> x!=0).collect(Collectors.toList());
-        if (res.isEmpty())  return true;
-        return false;
+        List<Integer> res= ht.values().stream().filter(v->v!=0).collect(Collectors.toList());
+        return res.size()==0;
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
         Hashtable<String, List<String>> ht = new Hashtable<>();
-        for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String ss = String.valueOf(arr);
+        for(String s: strs) {
+            char[] as = s.toCharArray();
+            Arrays.sort(as);
+            String ss = String.valueOf(as);
             List<String> located = ht.get(ss);
-            if( located!=null ) {
+            if(located!=null) {
                 located.add(s);
                 ht.put(ss, located);
             } else {
-                List<String> n = new ArrayList<>();
-                n.add(s);
-                ht.put(ss, n);
+                List<String> newone = new ArrayList<>();
+                newone.add(s);
+                ht.put(ss, newone);
             }
         }
-        return ht.values().stream().toList();
+        return ht.values().stream().collect(Collectors.toList());
     }
-
-    public int[] replaceElements(int[] arr) {
-        List<Integer> ans = new ArrayList<>();
-        int max1 = -1;
-        int L = arr.length;
-        for(int i=L-1; i>-1; i--) {
-            ans.add(max1);
-            max1 = Math.max(max1,arr[i]);
-        }
-        Collections.reverse(ans);
-        int[] aans = new int[ans.size()];
-        for(int i=0; i<L; i++) {
-            aans[i] = ans.get(i).intValue();
-        }
-        return aans;
-    }
-
-    public boolean isSubsequence(String s, String t) {
-        int L = s.length();
-        int located = 0;
-        int i=0, j=0;
-        while (j<t.length()) {
-            if (located==L) return true;
-            if (s.charAt(i)==t.charAt(j)) {
-                i++;
-                located++;
-            }
-            j++;
-        }
-        if(located==L) return true;
-        return false;
-    }
-
-    public int lengthOfLastWord(String s) {
-        String rs = s.trim();
-        int L = rs.length()-1;
-        int j = L;
-        while (j>-1) {
-            if(rs.charAt(j)==' ') break;
-            j--;
-        }
-        return (L-j);
-    }
-
-    public String longestCommonPrefix_1(String[] strs) {
-        String ans = "";
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length-1];
-        for(int i=0; i<Math.min(first.length(),last.length()); i++) {
-            if(first.charAt(i)!=last.charAt(i)) break;
-            ans+=first.charAt(i);
-        }
-        return ans;
-    }    
-
-    public String longestCommonPrefix_2(String[] strs) {    
-        if(strs==null || strs.length==0) return "";
-        String prefix = strs[0];
-        for (String s : strs) 
-            while(s.indexOf(prefix)!=0) 
-                prefix = prefix.substring(0, prefix.length()-1);
-        return prefix;
-    }
-
-    public int[] topKFrequent(int[] num, int k) {
-        int[] res = new int[k];
-        Map<Integer,Integer> memo = new HashMap<>();
-        for (int i : num) {
-            Integer located = memo.get(i);
-            if (located!=null) {
-                memo.put(i, located + 1);
-            } else {
-                memo.put(i, 1);
-            }
-        }
-        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(memo.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-        int j=0;
-        for(int i=list.size()-1; i>-1; i--) {
-            if (j==k) break;
-            Map.Entry<Integer,Integer> entry = list.get(i);
-            res[j++] = entry.getKey().intValue();
-        }
-        return res;
-    }
-
-    public String encode(List<String> strs) {
-        String ans = "";
-        for (String s: strs) {
-            for (char c: s.toCharArray()) {
-                int i = ((int) c) << 2;
-                ans += (char) i;
-            }
-            ans += (char) 10;
-        }
-        return ans;
-    }
-
-    public List<String> decode(String s) {
-        List<String> ans = new ArrayList<>();
-        String ele = "";
-
-        for (char c: s.toCharArray()) {
-            if(c==10) {
-                ans.add(ele);
-                ele = "";
-            } else {
-                int i = (int) c >> 2;
-                ele += (char) i;
-            }
-        }
-        return ans;
-    }
-
-    public int[] productExceptSelf(int[] nums) {
-        int[] ans= new int[nums.length];
-        int left = 1;
-        for(int i=0; i<nums.length; i++) {
-            ans[i] = left;
-            left = left * nums[i];
-        }
-        int right = 1;
-        for(int i= nums.length-1; i>-1; i--){
-            ans[i] = right * ans[i];
-            right = right * nums[i];
-        }
-        return ans;
-    }    
 
 }
